3c6e152723e9a148472a98128f45daa9c1788d49
Henrique Ferreira Nogueira <henrique.nogueira@rede.ulbra.br>
2020-03-12 21:44:18 -0300

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,7 +19,7 @@ describe('AppComponent', () => {
   it(`should have as title 'CadastroSP'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('CadastroSP');
+    expect(app.titulo).toEqual('CadastroSP');
   });
 
   it('should render title', () => {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'CadastroSP';
+  titulo = 'CadastroSP';
+  pessoas = [];
+  pessoa = ''
+  adicionar() {
+    this.pessoas.push(this.pessoa);
+    this.pessoa = null;
+  }
 }
